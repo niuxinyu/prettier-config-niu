235ca0db24ebc806da9b6afa52c4c73cd4ae7864
niuxinyu <2659546337@qq.com>
2023-10-10 18:39:02 +0800

    feat:import顺序

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,48 +1,67 @@
+const likeBuiltInModules = [
+  '^vue$',
+  '^vue-router$',
+  '^vuex$',
+  '^pinia$',
+  '^react$',
+  '^react-dom$',
+  '^react-router-dom$',
+]
 // v2
-const v2UiList = ['element-ui']
+const v2UiList = ['^element-ui(?!.*.(css|less|scss|sass|styl)$).*$']
 // v3
-const uiList = ['ant-design-vue', 'element-plus', 'naive-ui', 'view-ui-plus']
+const uiList = [
+  '^ant-design-vue(?!.*.(css|less|scss|sass|styl)$).*$',
+  '^element-plus(?!.*.(css|less|scss|sass|styl)$).*$',
+  '^naive-ui(?!.*.(css|less|scss|sass|styl)$).*$',
+  '^view-ui-plus(?!.*.(css|less|scss|sass|styl)$).*$',
+]
 
 // style
 const cssList = ['^[.@](.*)(css|less|scss|sass)$']
 
 module.exports = {
-	plugins: ['@trivago/prettier-plugin-sort-imports'],
-	printWidth: 100,
-	tabWidth: 2,
-	useTabs: true,
-	semi: false,
-	quoteProps: 'as-needed',
-	singleQuote: true,
-	jsxSingleQuote: true,
-	trailingComma: 'all',
-	bracketSpacing: true,
-	jsxBracketSameLine: false,
-	arrowParens: 'always',
-	requirePragma: false,
-	proseWrap: 'preserve',
-	htmlWhitespaceSensitivity: 'ignore',
-	vueIndentScriptAndStyle: false,
-	endOfLine: 'lf',
-	embeddedLanguageFormatting: 'auto',
-	importOrder: [
-		'<THIRD_PARTY_MODULES>',
-		...v2UiList,
-		...uiList,
-		// @ 开头的第三方包
-		'^@[^/](.*)$',
-		// 路径别名包 排除 css 文件
-		'^@/(?!.*.(css|less|scss|sass|styl)$).*$',
-		// 相对路径包
-		'^[./](?!.*.(css|less|scss|sass|styl)$).*$',
-		...cssList,
-	],
-	importOrderSeparation: true,
-	overrides: [
-		{
-			files: ['**/*.(css|less|scss|sass|styl)', '**/*.html'],
-			// files: ['**/*.css', '**/*.html'],
-			singleQuote: false,
-		},
-	],
+  plugins: ['@trivago/prettier-plugin-sort-imports'],
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  quoteProps: 'as-needed',
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'always',
+  requirePragma: false,
+  proseWrap: 'preserve',
+  htmlWhitespaceSensitivity: 'ignore',
+  vueIndentScriptAndStyle: false,
+  endOfLine: 'lf',
+  embeddedLanguageFormatting: 'auto',
+  importOrder: [
+    ...likeBuiltInModules,
+    '<THIRD_PARTY_MODULES>',
+    // ...v2UiList,
+    // ...uiList,
+    // @ 开头的第三方包
+    // '^@[^/](.*)$',
+    // 第三方包的样式文件
+    '^@[^/](.*)(css|less|scss|sass)$',
+    // 第三方包的样式文件
+    '^[^@/.](.*)(css|less|scss|sass)$',
+    // 路径别名包 排除 css 文件
+    '^@/(?!.*.(css|less|scss|sass|styl)$).*$',
+    // 相对路径包
+    '^[./](?!.*.(css|less|scss|sass|styl)$).*$',
+    ...cssList,
+  ],
+  importOrderSeparation: true,
+  overrides: [
+    {
+      files: ['**/*.(css|less|scss|sass|styl)', '**/*.html'],
+      // files: ['**/*.css', '**/*.html'],
+      singleQuote: false,
+    },
+  ],
 }
